2aeedbee888925609bb9bc5ad8ebe6d7a3bd297c
Serhii Choporov <s.choporoff@gmail.com>
2022-06-24 17:46:41 +0300

    the standard deviation

--- analysis/contour_mgs.py
+++ analysis/contour_mgs.py
@@ -18,15 +18,15 @@ from lattice.torus import plot_matrix_colorbar, plot_matrix_values
 from analysis.statistics import SeriesStatistics
 
 # parameters of the simulation
-length = 21  # length of the social space
-density = 0.3  # density of spots randomly occupied by agents
+length = 10  # length of the social space
+density = 0.7  # density of spots randomly occupied by agents
 initial_percent = 0.3  # initial percent of contributing agents
-tick_max = 50  # the maximum number of attempts at one simulation
-Ngrid = 11  # number of points in ranges for synergy and pressure
+tick_max = 40  # the maximum number of attempts at one simulation
+Ngrid = 21  # number of points in ranges for synergy and pressure
 vmax = 120000  # the maximum value at legends for frequencies
 use_groups = True  # if True then the simulation uses the frequency of behaviour changes in groups
 epsilon = 10.0  # outliers level for statistics
-stop_on_adoption = False  # if False then the simulation isn't stopped on all contributors of all non-contributors
+stop_on_adoption = True  # if False then the simulation isn't stopped on all contributors of all non-contributors
 # create ranges for the simulation
 syn = np.linspace(0, 10, Ngrid)  # grid nodes for synergy
 pre = np.linspace(0, 10, Ngrid)  # grid nodes for pressure
--- analysis/statistics.py
+++ analysis/statistics.py
@@ -35,11 +35,14 @@ class SeriesStatistics:
         print(f"number of synergy-pressure pairs with more than {100.0 - self.epsilon}% adoption: {self.upper_epsilon}")
         print(f"number of synergy-pressure pairs with adoption in [{self.epsilon}%; {100.0 - self.epsilon}%]: {self.non_outlier}")
         print(f"average percent of contributors excluding with adoption in [{self.epsilon}%; {100.0 - self.epsilon}%]: {np.mean(self.non_outlier_percent)}")
+        print(f"the standard deviation for the percent of contributors excluding with adoption in [{self.epsilon}%; {100.0 - self.epsilon}%]: {np.std(self.non_outlier_percent)}")
         print(f"minimum number of ticks to get the equilibrium with less than {self.epsilon}% adoption: {np.min(self.below_epsilon_starting_tick) if self.below_epsilon_starting_tick else None}")
         print(f"average number of ticks to get the equilibrium with less than {self.epsilon}% adoption: {np.mean(self.below_epsilon_starting_tick) if self.below_epsilon_starting_tick else None}")
+        print(f"the standard deviation for the number of ticks to get the equilibrium with less than {self.epsilon}% adoption: {np.std(self.below_epsilon_starting_tick) if self.below_epsilon_starting_tick else None}")
         print(f"maximum number of ticks to get the equilibrium with less than {self.epsilon}% adoption: {np.max(self.below_epsilon_starting_tick) if self.below_epsilon_starting_tick else None}")
         print(f"minimum number of ticks to get the equilibrium with more than {100.0 - self.epsilon}% adoption: {np.min(self.upper_epsilon_starting_tick) if self.upper_epsilon_starting_tick else None}")
         print(f"average number of ticks to get the equilibrium with more than {100.0 - self.epsilon}% adoption: {np.mean(self.upper_epsilon_starting_tick) if self.upper_epsilon_starting_tick else None}")
+        print(f"the standard deviation for the number of ticks to get the equilibrium with more than {100.0 - self.epsilon}% adoption: {np.std(self.upper_epsilon_starting_tick) if self.upper_epsilon_starting_tick else None}")
         print(f"maximum number of ticks to get the equilibrium with more than {100.0 - self.epsilon}% adoption: {np.max(self.upper_epsilon_starting_tick) if self.upper_epsilon_starting_tick else None}")
         print("---")
 
@@ -51,22 +54,28 @@ def print_stats(contrib_space: np.ndarray, ticks_space: np.ndarray, epsilon: flo
     non_contributors_number = len(non_contrib_idx[0])
     mean_run_contributors_percent = np.mean(contrib_space[np.where((epsilon <= contrib_space) & (contrib_space <= 100.0 - epsilon))])
     average_ticks_number = np.mean(ticks_space)
+    std_ticks_number = np.std(ticks_space)
     contrib_ticks_number = ticks_space[contrib_idx]
     average_ticks_contrib_number = np.mean(contrib_ticks_number) if len(contrib_ticks_number) > 0 else None
+    std_ticks_contrib_number = np.std(contrib_ticks_number) if len(contrib_ticks_number) > 0 else None
     min_ticks_contrib_number = np.min(contrib_ticks_number) if len(contrib_ticks_number) > 0 else None
     max_ticks_contrib_number = np.max(contrib_ticks_number) if len(contrib_ticks_number) > 0 else None
     non_contrib_ticks_number = ticks_space[non_contrib_idx]
     average_ticks_non_contrib_number = np.mean(non_contrib_ticks_number) if len(non_contrib_ticks_number) > 0 else None
+    std_ticks_non_contrib_number = np.std(non_contrib_ticks_number) if len(non_contrib_ticks_number) > 0 else None
     min_ticks_non_contrib_number = np.min(non_contrib_ticks_number) if len(non_contrib_ticks_number) > 0 else None
     max_ticks_non_contrib_number = np.max(non_contrib_ticks_number) if len(non_contrib_ticks_number) > 0 else None
     print(f"number of synergy-pressure pairs with less than {epsilon} adoption: {non_contributors_number}")
     print(f"number of synergy-pressure pairs with more than {100.0 - epsilon} adoption: {contributors_number}")
     print(f"average percent of contributors excluding with adoption in [{epsilon}; {100.0 - epsilon}]: {mean_run_contributors_percent}")
     print(f"average number of ticks in the simulation: {average_ticks_number}")
+    print(f"the standard deviation of the number of ticks in the simulation: {std_ticks_number}")
     print(f"minimum number of ticks to get less than {epsilon} adoption: {min_ticks_non_contrib_number}")
     print(f"average number of ticks to get less than {epsilon} adoption: {average_ticks_non_contrib_number}")
+    print(f"the standard deviation of the number of ticks to get less than {epsilon} adoption: {std_ticks_non_contrib_number}")
     print(f"maximum number of ticks to get less than {epsilon} adoption: {max_ticks_non_contrib_number}")
     print(f"minimum number of ticks to get more than {100.0 - epsilon} adoption: {min_ticks_contrib_number}")
     print(f"average number of ticks to get more than {100.0 - epsilon} adoption: {average_ticks_contrib_number}")
+    print(f"the standard deviation of the number of ticks to get more than {100.0 - epsilon} adoption: {std_ticks_contrib_number}")
     print(f"maximum number of ticks to get more than {100.0 - epsilon} adoption: {max_ticks_contrib_number}")
     print("---")
